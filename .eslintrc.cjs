565a88612a61bd4914f031b12fdcb405f44c492a
Mohammad Toosi <m_dd@rogers.com>
2023-02-11 15:13:51 +0330

    fix: eslint not detect vue files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,4 +5,9 @@ module.exports = {
     browser: true,
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
 };
