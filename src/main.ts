c065efe39891445fde29098752e198d2f5ea40b9
Mohammad Toosi <m_dd@rogers.com>
2023-02-11 19:32:50 +0330

    feat: add primevue and tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
-import "./style.css";
+import PrimeVue from "primevue/config";
 import App from "./App.vue";
+
+// styles
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "./style.css";
+
+const app = createApp(App);
+
+app.use(PrimeVue);
 
 createApp(App).mount("#app");
