382365bf56a23dd61bf929e33c728258b8b8085d
Mohammad Toosi <m_dd@rogers.com>
2023-02-14 05:43:25 +0330

    chore: finish theme-changer test

--- src/components/theme-changer/__tests__/theme-changer.spec.ts
+++ src/components/theme-changer/__tests__/theme-changer.spec.ts
@@ -1,14 +1,36 @@
 import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/vue";
+import { fireEvent, render, screen } from "@testing-library/vue";
+import PrimeVue from "primevue/config";
 import ThemeChanger from "../theme-changer.vue";
+import router from "@/router";
+import { themeStore } from "@/store/theme/theme-store";
+
+const renderComponent = (props = {}) =>
+  render(ThemeChanger, {
+    global: {
+      plugins: [PrimeVue, router],
+    },
+    props,
+  });
 
 describe("ThemeChanger", () => {
   it("renders properly", () => {
-    render(ThemeChanger);
+    renderComponent(ThemeChanger);
     expect(screen.getByText(/select theme/i)).toBeTruthy();
   });
 
-  // it("should render change the theme", () => {
-  //   render(ThemeChanger);
-  // });
+  it("should render change the theme", async () => {
+    renderComponent(ThemeChanger);
+
+    // open themes
+    await fireEvent.click(screen.getByText(/select theme/i));
+
+    // check light theme is exists
+    const light = screen.getByText(/light/i);
+    expect(light).toBeTruthy();
+
+    // click on the light theme
+    await fireEvent.click(light);
+    expect(themeStore.selectedThem.value).toBe("light");
+  });
 });
